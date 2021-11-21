f100b3c7ca489ec081405c604ebbc7854d130de2
kritika <bansalk128@gmail.com>
2021-11-21 12:37:17 +0530

    second commit

---
+++ bb.js
@@ -0,0 +1,6 @@
+const element = document.querySelector(".xx");
+
+element.addEventListener("click", () => {
+    e.preventDefault();
+    window.location = "https://github.com/kaybee128/DuckHunt-ByKritika";    
+});
